3427538619962f7c34b3ac7558453d548b4519d6
SayeedSanjana <sanjanasayeed68@gmail.com>
2025-02-09 18:07:31 -0500

    job interface created and implemantation is also written

--- jobapp/src/main/java/com/embarkx/jobapp/job/JobController.java
+++ jobapp/src/main/java/com/embarkx/jobapp/job/JobController.java
@@ -1,15 +1,31 @@
 package com.embarkx.jobapp.job;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@RestController
 public class JobController {
-    List<Job> jobs = new ArrayList<Job>();
+    JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
 
     @GetMapping("/jobs")
     public List<Job> findAll(){
-        return jobs;
+        return jobService.findAll();
+    }
+
+    @PostMapping("/create-jobs")
+    public String createJob(@RequestBody Job job){
+        jobService.createJob(job);
+        return "Job Added Successfully";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public Job getJobById(@PathVariable Long id){
+        return jobService.getJobById(id);
     }
 }
---
+++ jobapp/src/main/java/com/embarkx/jobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,30 @@
+package com.embarkx.jobapp.job.impl;
+
+import com.embarkx.jobapp.job.Job;
+import com.embarkx.jobapp.job.JobService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JobServiceImpl implements JobService {
+    List<Job> jobs = new ArrayList<Job>();
+    @Override
+    public List<Job> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void createJob(Job job) {
+
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        for(Job job : jobs) {
+            if(job.getId().equals(id)){
+                return job;
+            }
+        }
+        return null;
+    }
+}
