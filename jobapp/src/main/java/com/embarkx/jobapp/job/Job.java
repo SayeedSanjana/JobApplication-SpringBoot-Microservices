93ce91cc84b394b9ecc117e8c8fff30e3d50efbe
SayeedSanjana <sanjanasayeed68@gmail.com>
2025-02-09 20:38:15 -0500

    jpa with h2 databases

--- jobapp/src/main/java/com/embarkx/jobapp/job/Job.java
+++ jobapp/src/main/java/com/embarkx/jobapp/job/Job.java
@@ -1,6 +1,13 @@
 package com.embarkx.jobapp.job;
 
-public class Job{
+import jakarta.persistence.*;
+
+@Entity
+//@Table(name="job_table")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
@@ -8,19 +15,24 @@ public class Job{
     private String maxSalary;
     private String location;
 
-    public Job(Long id,String title, String description,String minSalary,
+    public Job(Long id, String title, String description, String minSalary,
                String maxSalary, String location) {
         this.id = id;
-        this.title=title;
-        this.description=description;
-        this.minSalary=minSalary;
-        this.maxSalary=maxSalary;
-        this.location=location;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public Job() {
+
     }
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
@@ -32,30 +44,38 @@ public class Job{
     public void setTitle(String title) {
         this.title = title;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public String getMinSalary() {
         return minSalary;
     }
+
     public void setMinSalary(String minSalary) {
         this.minSalary = minSalary;
     }
+
     public String getMaxSalary() {
         return maxSalary;
     }
+
     public void setMaxSalary(String maxSalary) {
         this.maxSalary = maxSalary;
     }
+
     public String getLocation() {
         return location;
     }
+
     public void setLocation(String location) {
         this.location = location;
     }
-}
 
+}
 
